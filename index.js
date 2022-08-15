1748a5c11c1744bf8f83253cf2cbbdd5bb41cc47
Conner Ow <connerow1115@gmail.com>
2022-08-15 09:02:49 -0500

    burp

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -27,7 +28,40 @@ mongoose.connect(
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("JSON MongoDB CRUD API");
+});
+
+app.post("/query", async (req, res) => {
+  let outputObj = {};
+  let [id, fields] = Object.entries(req.body)[0];
+  let data = await Data.findOne({ _id: id });
+  if (data) {
+    for([key, value] of Object.entries(data.data)) {
+      if(fields.split(",").includes(key)) {
+        outputObj[key] = value;
+      }
+    }
+    res.status(200).send(outputObj);
+  }else{
+    res.status(404).json({})
+  }
+  
+});
+
+app.post("/mutate", async (req, res) => {
+  let data = await Data.findOne({ _id: req.body.id });
+  let mutations = req.body.mutations;
+  if (data) {
+    Data.findByIdAndUpdate(req.body.id, { $set: Object.fromEntries(Object.entries(mutations).map(x => ["data." + x[0], x[1]])) }, { new: true }, (err, doc) => {
+      if(err){
+        res.status(500).json({})
+        return;
+      }
+      res.json(doc.data);
+    });
+  }else{
+    res.status(404).json({})
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
